6db2162222eea54594d5cc615c6f83664bda2a76
Jarno Rajahalme <jarno@isovalent.com>
2021-08-22 12:05:53 +0300

    install: Support ":latest" version, tags in user image
    Do not add the version tag if user supplied image reference already
    has a tag. Simply concatenate the user version to the image if it
    already contains a colon. These changes are useful for CI built PR
    images, e.g.,
    'cilium install --version -ci:4fac771179959ca575eb6f993d566653d3bfa167'.
    
    "latest" tag is also supported, when prepended with a colon:
    'cilium install --version :latest'.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -11,7 +11,9 @@ func TestCheckVersion(t *testing.T) {
 		want bool
 	}{
 		{"v1.9.6", true},
+		{"v1.9.6.5", false},
 		{"1.9.6", true},
+		{"1.9.6.5", false},
 		{"v1.10.0-rc1", true},
 		{"1.10.0-rc1", true},
 		{"1.9", false},
@@ -24,6 +26,11 @@ func TestCheckVersion(t *testing.T) {
 		{"ddd", false},
 		{"v.1.9", false},
 		{"v..1.9", false},
+		{":latest", true},
+		{":92ff7ffa762f6f8bc397a28e6f3147906e20e8fa", true},
+		{":92ff7ffa762f6f8bc397a28e6f3147906e20e8fa@sha256:4fde4abc19a1cbedb5084f683f5d91c0ea04b964a029e6d0ba43961e1ff5b5d8", true},
+		{"-ci:92ff7ffa762f6f8bc397a28e6f3147906e20e8fa", true},
+		{"-ci:92ff7ffa762f6f8bc397a28e6f3147906e20e8fa@sha256:4fde4abc19a1cbedb5084f683f5d91c0ea04b964a029e6d0ba43961e1ff5b5d8", true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
